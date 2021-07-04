922867b804812c47b15673dc59fc0dfad373f293
balaji <balajikrishnan00@gmail.com>
2021-07-04 22:54:39 +0300

    commit

---
+++ 01/34_Practies.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+from kivy.base import Builder
+from kivy.core.window import Window
+Window.size=(320,480)
+kv= """
+
+BoxLayout:
+    orientation:'vertical'
+    padding:10
+    spacing:12
+    TextInput:
+        
+        
+    TextInput:
+    TextInput:
+    TextInput:
+    TextInput:
+    Button:
+        text:'Create An Account'
+        
+    Label:
+        text:'Already have an Account?[b] Sign in[/b]'
+        markup:True
+        
+    
+    
+
+"""
+
+
+class MainApp(App):
+    def build(self):
+        return Builder.load_string(kv)
+MainApp().run()
