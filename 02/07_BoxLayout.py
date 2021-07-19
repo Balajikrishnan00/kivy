f0b8876252eb53a42ff4032454f5103d5c8f711b
balaji <balajikrishnan00@gmail.com>
2021-07-19 20:15:46 +0300

    file updated

---
+++ 02/07_BoxLayout.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
+
+class myanchor(AnchorLayout):
+    pass
+
+class hibutton(BoxLayout):
+    pass
+class wel(BoxLayout):
+    pass
+class Second(BoxLayout):
+    def __init__(self,**kwargs):
+        super(Second, self).__init__(**kwargs)
+        #self.padding=100
+        b1=Button(text='A')
+        self.add_widget(b1)
+
+class First(Widget):
+    pass
+
+class Boxapp(App):
+    pass
+
+Boxapp().run()
---
+++ 02/Button.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class hello(Widget):
+    pass
+
+class buttonapp(App):
+    pass
+buttonapp().run()
