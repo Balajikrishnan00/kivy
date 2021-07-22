b6f5ee7c753b453773d24c78d6b265e1112e36fb
balaji <balajikrishnan00@gmail.com>
2021-07-22 10:27:33 +0300

    updated

--- 04_KivyMD/02_TextInput.py
+++ 04_KivyMD/02_TextInput.py
@@ -1,10 +1,24 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
+from kivy.uix.image import Image
+from kivy.uix.gridlayout import GridLayout
+
+
+class myimage(GridLayout):
+    pass
+
 
 class mybox(BoxLayout):
-    pass
+    textinput_text = StringProperty('Foo')
+
+    def ontext_validate(self, widget):
+        self.textinput_text = widget.text
+
 
 class tesapp(App):
     def build(self):
-        return mybox()
+        return myimage()
+
+
 tesapp().run()
