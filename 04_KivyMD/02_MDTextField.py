13c74c474aed2ad20495615e1aaecdc7ec4ee5a7
balaji <balajikrishnan00@gmail.com>
2021-07-22 22:23:38 +0300

    updated

---
+++ 04_KivyMD/02_MDTextField.py
@@ -0,0 +1,26 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+
+KV="""
+MDFloatLayout:
+    MDTextField:
+        hint_text: "MDTextField"
+        size_hint_x:.5
+        pos_hint:{'center_x':.5,'center_y':.8}
+    MDTextFieldRound:
+        hint_text: "MDTextFieldRound"
+        size_hint_x:.5
+        pos_hint:{'center_x':.5,'center_y':.6}
+    MDTextFieldRect:
+        hint_text: "MDTextFieldRect"
+        size_hint_y:.1
+        size_hint_x:.5
+        pos_hint:{'center_x':.5,'center_y':.4}
+        
+"""
+
+class TextField(MDApp):
+    def build(self):
+        long=Builder.load_string(KV)
+        return long
+TextField().run()
